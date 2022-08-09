c68f63321312073dc971bb6e74ffb7f62cf6dc14
lesson1 <bayaliev07@gmail.com>
2022-08-09 19:36:14 +0300

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,28 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> array = new ArrayList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 50; i++) {
+            int a = random.nextInt(100);
+            list.add(a);
+            if (a %2==1){
+                array.add(a);
+            } else if (a %2==0) {
+                arrayList.add(a);
+            }
+        }
+        System.out.println(list);
+        System.out.println("четные - " + arrayList);
+        System.out.println("нечетныe" + " - "+array);
+
+        }
     }
-}
